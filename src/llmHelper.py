cb4064cab5ad40657dbb1650e9010f0f0d577c07
Abishek Dound <145567841+abhishekdound@users.noreply.github.com>
2026-03-24 16:26:30 +0530

    Cleanup llmHelper.py by removing unused settings
    Removed environment variable settings and warning filters.

--- src/llmHelper.py
+++ src/llmHelper.py
@@ -20,17 +20,6 @@ from typing import NotRequired
 
 
 
-os.environ["TOKENIZERS_PARALLELISM"] = "false"
-os.environ["HF_HUB_DISABLE_SYMLINKS_WARNING"] = "1"
-os.environ["HF_HUB_DISABLE_PROGRESS_BARS"] = "1"
-
-from transformers.utils import logging
-logging.set_verbosity_error()
-
-
-import warnings
-warnings.filterwarnings("ignore", message=".*TypedStorage is deprecated.*")
-warnings.filterwarnings("ignore", category=UserWarning, module="torch.nn.modules.module")
 
 
 
